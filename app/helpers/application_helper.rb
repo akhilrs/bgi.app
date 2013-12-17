384c599c4133c325662eec2b44d748e4867c1f36
Akhil R S <akhilrs@live.com>
2013-12-17 22:16:59 +0530

    Modified style , links etc

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def title
+		base_title = 'BGI RoR App'
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
+
+	def logo
+		image_tag("logo.jpg", :alt => "BGI RoR App", :class => "round")
+	end
 end
