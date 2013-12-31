fc286c53e3fe8f41fff440c97c73851645206ad7
anishkrish <anishkris@gmail.com>
2013-12-31 13:45:21 +0530

    Modified user migration

--- db/migrate/20131231063417_devise_create_users.rb
+++ db/migrate/20131231063417_devise_create_users.rb
@@ -8,8 +8,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :encrypted_password, :null => false, :default => ""
       t.text :addr1
       t.text :addr2
-      t.integer :state, :limit => 6
-      t.integer :country, :limit => 6
+      t.belongs_to :state
+      t.belongs_to :country
       t.string :zipcode, :limit => 10
 
       ## Recoverable
@@ -27,10 +27,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
@@ -43,7 +43,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
-    # add_index :users, :confirmation_token,   :unique => true
+    add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
   end
 end
