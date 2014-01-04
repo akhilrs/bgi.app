b5d774ff86b22e8e4b58b1b19c62f2fb3bf9ae3f
Akhil R S <akhilrs@live.com>
2014-01-04 11:08:47 +0530

    Modified devise Registration controller and started adding new user create login in registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,25 @@
 class RegistrationsController < Devise::RegistrationsController
 	def create
-		@password = sign_up_params
+		@params = sign_up_params
+			@userpara = {'email' => 'akhil@breezegoindia.com', 'password' => @params["password"],
+				'fname' => 'Akhil'}
+			build_resource(@userpara)
+			if resource.save
+				yield resource if block_given?
+			      if resource.active_for_authentication?
+			        #set_flash_message :notice, :signed_up if is_flashing_format?
+			        sign_up(resource_name, resource)
+			        #respond_with resource, :location => after_sign_up_path_for(resource)
+			        responds = {'status' =>'success'}
+			      else
+			        #set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+			        expire_data_after_sign_in!
+			        #respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+			        responds = {'status' => 'failed'}
+			      end
+			else
+				responds = {'status' => 'failed'}
+			end
+		render json: responds.to_json
   	end
 end
