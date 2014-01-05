068d61c4f7fb6dc18bbe24332707c2068cc1aab6
Akhil R S <akhilrs@live.com>
2014-01-05 10:00:37 +0530

    Added google auth code

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -228,7 +228,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   config.omniauth :facebook, '434639389998049', 'fda773651a8033a56879b0bb914621b6', :scope => 'user,public_repo'
-
+  require "omniauth-google-oauth2"
+  config.omniauth :google_oauth2, '1025776291973-i2ctggcuoi06f2nspcns22q7dppbk9vq.apps.googleusercontent.com', 'f9XrU0P1FmjBDggRxh_gsPWf', { access_type: "offline", approval_prompt: "" }
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
