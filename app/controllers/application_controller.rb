de6e858214893ba5a66666a07bfeab6f56b36283
Akhil R S <akhilrs@live.com>
2014-01-12 12:38:15 +0530

    Modified devise sign_redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,24 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  after_filter :store_location
+
    
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:fname, :lname, :email, :image, :password, :password_confirmation, :facebook, :google_oauth2, :gid, :fid, :number, :make, :model, :imei, :serialno) }
   end
+
+  def store_location
+  	# store last url - this is needed for post-login redirect to whatever the user last visited.
+	  if (request.fullpath != "/users/sign_in" &&
+	      request.fullpath != "/users/sign_up" &&
+	      request.fullpath != "/users/password" &&
+	      !request.xhr?) # don't store ajax calls
+	    session[:previous_url] = request.fullpath 
+	  end
+  end
+
+  	def after_sign_in_path_for(resource)
+  		session[:previous_url] || root_path
+	end
 end
