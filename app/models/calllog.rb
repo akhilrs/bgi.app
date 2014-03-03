22385f15b7fa42896e57ac74f4e0ac3c5e529ece
anishkrish <anishkris@gmail.com>
2014-03-03 14:38:33 +0530

    Modified duration

--- app/models/calllog.rb
+++ app/models/calllog.rb
@@ -5,13 +5,13 @@ class Calllog < ActiveRecord::Base
 		@phone = Phone.find_by_serialno(serialno)
 		name = (params[:name] != "") ? params[:name] : ""
 		if !@phone.nil?
-			#duration = Time.at((params[:duration]).to_i).utc.strftime("%H:%M:%S")
+			duration = Time.at((params[:duration]).to_i).utc.strftime("%H:%M:%S")
 			res = @phone.calllogs.create(
 								number: params[:number],
 								name: name,
 								calltype: params[:type],
 								datetime: Time.parse(params[:datetime]).utc,
-								duration: params[:duration]
+								duration: duration
 								)
 			response = {'status' => 'success', 'message' => 'Added successfully'}
 		else
